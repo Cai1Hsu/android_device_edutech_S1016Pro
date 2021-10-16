9befbfc0d8ce88e07124ec892a70ec99cc38d7f6
Caiyi Hsu <caiyi@pop-os.localdomain>
2021-10-16 16:35:46 +0800

    add files

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rk3368H_64.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rk3368H_64-user \
+    omni_rk3368H_64-userdebug \
+    omni_rk3368H_64-eng
---
+++ omni_rk3368H_64.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from rk3368H_64 device
+$(call inherit-product, device/rockchip/rk3368H_64/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := rk3368H_64
+PRODUCT_NAME := omni_rk3368H_64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := S1016Pro
+PRODUCT_MANUFACTURER := rockchip
